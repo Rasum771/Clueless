2f57d18be19ce5244fcc72dff1adb6aa173317bc
Abdiaziz <abdiazizjama44@gmail.com>
2024-07-13 14:49:58 -0500

    Nigger2.0

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-'''from openai import OpenAI as openai
 
-# Point to the local server
-openai.api_base = "http://localhost:1234/v1"
-openai.api_key = "lm-studio"
-
-# Initialize the system message
-def create_chat_completion(user_input, system_message):
-    return openai.ChatCompletion.create(
-        model="local-model",
-        messages=[
-            
-         {"role" : "system", "content" : system_message},
-         {"role" : "user", "content" : user_input}
-        ], temperature=0.7
-    )
-
-# Start the chat loop
-while True:
-    system_message = ("You are an assistant or a chatbot", "Respond in a helpful and concise manner")
-    user_input = input("You: ")
-    if user_input.lower() in ["exit", "quit"]:
-        break
-
-    # Create the chat completion request
-    completion = create_chat_completion(user_input, system_message)
-
-    print("Model Responses: ", completion.choices[0].message.content)
-
-    # Print the response from the model '''
- 
 from openai import OpenAI
 
 # Point to the local server
